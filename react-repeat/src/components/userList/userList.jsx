8a0029976f4e38c69cef8d0cebaa4827a83ce388
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-02-20 18:19:44 +0900

    Change userlist design

--- react-repeat/src/components/userList/userList.jsx
+++ react-repeat/src/components/userList/userList.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 const UserList = ({users}) => {
     return (
         <div>
+            
             {users.map(user=>{
                 return(
-                    <div key={user.id}>{user.name}</div>
+                    <div className="card mb-2" key={user.id} >
+                <div className="card-body pd-3">
+                    {user.name}
+                </div>
+            </div>
                 )
             })}
         </div>
